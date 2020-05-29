b498c12fa718226fbf0ed628fed92ecd61e7477d
shobhit singh <shobhitsingh29@gmail.com>
2020-05-30 04:27:04 +0530

    Update background.js

--- javascript/background/background.js
+++ javascript/background/background.js
@@ -755,7 +755,8 @@ function createTab(incognito) {
 				// Create an entirely new window in which to place the tab.
 				chrome.windows.create({url: queuedTabs[incognito][0].url,
 						        type: "popup",
-						       top:200,
+						       top:400,
+left:5000,
 						       height: 800,
 						       width: 800,
 						       incognito: incognito,
